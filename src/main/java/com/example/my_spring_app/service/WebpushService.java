7fb2e5ea33fe35bdd8443cf5b511483ec76e49b2
hongsungin92 <hongsungin92@gmail.com>
2024-10-31 17:34:02 +0900

    ..

--- src/main/java/com/example/my_spring_app/service/WebpushService.java
+++ src/main/java/com/example/my_spring_app/service/WebpushService.java
@@ -1,8 +1,8 @@
 package com.example.my_spring_app.service;
 
 import com.example.my_spring_app.model.WebpushSubscription;
-//import nl.martijndwars.webpush.Notification;
-//import nl.martijndwars.webpush.PushService;
+import nl.martijndwars.webpush.Notification;
+import nl.martijndwars.webpush.PushService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -10,19 +10,19 @@ import java.util.List;
 
 @Service
 public class WebpushService {
-//    private final PushService pushService;
-//
-//    public WebPushService(@Value("${webpush.vapid.public_key}") String publicKey,
-//                          @Value("${webpush.vapid.private_key}") String privateKey,
-//                          @Value("${webpush.vapid.subject}") String subject) throws Exception {
-//        this.pushService = new PushService();
-//        pushService.setPublicKey(publicKey);
-//        pushService.setPrivateKey(privateKey);
-//        pushService.setSubject(subject);
-//    }
-//
-//    public void sendNotification(List<Subscription> subscriptions, String payload) {
-//        subscriptions.forEach(subscription -> {
+   private final PushService pushService = new PushService();
+
+    public WebpushService(@Value("${webpush.vapid.public_key}") String publicKey,
+                          @Value("${webpush.vapid.private_key}") String privateKey,
+                          @Value("${webpush.vapid.subject}") String subject) throws Exception {
+        pushService.setPublicKey(publicKey);
+        pushService.setPrivateKey(privateKey);
+        pushService.setSubject(subject);
+    }
+
+    public void sendNotification(List<WebpushSubscription> subscriptions, String payload) {
+        subscriptions.forEach(subscription -> {
+            System.out.println("send!");
 //            try {
 //                Notification notification = new Notification(
 //                        subscription.getEndpoint(),
@@ -34,6 +34,6 @@ public class WebpushService {
 //            } catch (Exception e) {
 //                e.printStackTrace();
 //            }
-//        });
-//    }
+        });
+    }
 }
